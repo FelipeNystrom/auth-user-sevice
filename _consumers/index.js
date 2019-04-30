afd3bbea17934173ab45556fc81f0ee71ffcdac5
Felipe Nystrom <felipe@pelif.se>
2019-04-30 05:34:28 +0200

    add kafkainit at server config

--- _consumers/index.js
+++ _consumers/index.js
@@ -3,7 +3,7 @@ const kafka = require('kafka-node');
 const HighLevelProducer = kafka.HighLevelProducer;
 const Client = kafka.KafkaClient;
 const Consumer = kafka.Consumer;
-const { checkIfUserExists: getUser } = require('../_queries');
+const { getUserWithUsername } = require('../_queries');
 
 const client = new Client();
 const producer = new HighLevelProducer(client, { requireAcks: 1 });
@@ -12,8 +12,13 @@ const topics = [{ topic: 'author', partition: 0 }];
 
 const consumer = new Consumer(client, topics);
 
+producer.on('ready', () => {
+  console.log('Producer is ready');
+});
+
 consumer.on('message', (err, data) => {
   if (err) {
+    debugger;
     console.error(err);
   }
   console.log(data);
--- serverConfig/index.js
+++ serverConfig/index.js
@@ -1,10 +1,41 @@
 const express = require('express');
 const sanitize = require('sanitize');
 const passport = require('passport');
+const HighLevelProducer = kafka.HighLevelProducer;
+const Client = kafka.KafkaClient;
+const client = new Client({ kafkaHost: 'kafka:9092' });
+const producer = new HighLevelProducer(client);
+
+producer.on('error', err => {
+  console.log('kafka producer has an error: ');
+  console.error(err);
+});
+
+producer.on('ready', () => {
+  console.log('kafka producer is ready');
+});
+debugger;
+consumer.on('message', (err, data) => {
+  debugger;
+  if (err) {
+    debugger;
+    console.error(err);
+  }
+  console.log(data);
+  debugger;
+});
+
+const kafkaInit = (req, res, next) => {
+  req.producer = producer;
+  req.client = client;
+  next();
+  debugger;
+};
 
 module.exports = server => {
   server.use(sanitize.middleware);
   server.use(express.json());
   server.use(express.urlencoded({ extended: true }));
   server.use(passport.initialize());
+  server.use(kafkaInit);
 };
