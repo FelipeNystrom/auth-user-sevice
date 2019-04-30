865fb0b1481c76d2485f840a590f7cac8233e2f0
Felipe Nystrom <felipe@pelif.se>
2019-04-30 13:49:21 +0200

    consumer working, problem with reply producer sending to topic

--- _consumers/index.js
+++ _consumers/index.js
@@ -1,26 +1,48 @@
-const kafka = require('kafka-node');
-
-const HighLevelProducer = kafka.HighLevelProducer;
-const Client = kafka.KafkaClient;
-const Consumer = kafka.Consumer;
 const { getUserWithUsername } = require('../_queries');
 
-const client = new Client();
-const producer = new HighLevelProducer(client, { requireAcks: 1 });
+const { Kafka } = require('kafkajs');
 
-const topics = [{ topic: 'author', partition: 0 }];
-
-const consumer = new Consumer(client, topics);
-
-producer.on('ready', () => {
-  console.log('Producer is ready');
+const kafka = new Kafka({
+  clientId: 'authService',
+  brokers: ['kafka:9092']
 });
 
-consumer.on('message', (err, data) => {
-  if (err) {
-    debugger;
-    console.error(err);
-  }
-  console.log(data);
-  debugger;
-});
+debugger;
+
+const consumer = kafka.consumer({ groupId: 'auth-group' });
+const producer = kafka.producer();
+
+const consume = async topic => {
+  let messageFromConsumer;
+
+  await consumer.connect();
+  await producer.connect();
+
+  await consumer.subscribe(topic);
+  await consumer.run({
+    eachMessage: async ({ topic, partition, message }) => {
+      const prefix = `${topic}[${partition} | ${message.offset}] / ${
+        message.timestamp
+      }`;
+      messageFromConsumer = message.value.toString('utf8');
+      console.log(messageFromConsumer);
+      console.log(`- ${prefix} ${message.key}#${message.value}`);
+
+      const usernameFromDb = await getUserWithUsername(messageFromConsumer);
+
+      console.log(usernameFromDb);
+
+      await producer.send({
+        topic: 'author_reply',
+        messages: [{ value: Buffer.from(usernameFromDb) }],
+        acks: 1
+      });
+
+      producer.disconnect();
+    }
+  });
+};
+
+module.exports = topic => {
+  return consume(topic);
+};
--- server.js
+++ server.js
@@ -4,9 +4,14 @@ const passport = require('passport');
 const passportConfig = require('./_auth-service');
 const serverConfig = require('./serverConfig');
 const mountRoutes = require('./_routes');
+const kafkaStandaloneConsumerSetup = require('./_consumers');
 const _port = process.env.PORT || 5001;
 
+const topic = { topic: 'author' };
+
 serverConfig(server);
+kafkaStandaloneConsumerSetup(topic);
+debugger;
 passportConfig(passport);
 mountRoutes(server);
 
