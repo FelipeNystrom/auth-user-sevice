ae76be5dd4e65dc78e206dc0f5421655d1fa3a14
Felipe Nystrom <felipe@pelif.se>
2019-05-16 14:05:56 +0200

    removes passport packages and auth folder

--- _auth-service/index.js
+++
@@ -1,7 +0,0 @@
-const loginStrategy = require('./loginStrategy');
-const jwtStrategy = require('./jwtStrategy');
-
-module.exports = passport => {
-  passport.use('login', loginStrategy);
-  passport.use('jwt', jwtStrategy);
-};
